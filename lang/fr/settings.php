90548b4f35ac9651593b35956d5717a06e6ea17e
Nicolas Friedli <nicolas@theologique.ch>
2014-01-16 15:20:25 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['output']                = 'Afficher le PDF dans le navigateur ou demander à le télécharger?';
+$lang['usecache']              = 'Garder le PDF en cache?';
+$lang['template']              = 'Template XSL par défaut. Il peut être remplacé page par page avec <code>&tpl=name.xsl</code> à la fin de l\'URL.';
+$lang['command']               = 'La commande, où <code>{xml}</code>, <code>{xsl}</code>, and <code>{pdf}</code> seront remplacés par les chemins d\'accès corrects du système.';
