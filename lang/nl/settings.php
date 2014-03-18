af1a003086cc64d669050ec6e2fea34372068dfb
Rene <wllywlnt@yahoo.com>
2014-03-18 15:30:04 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Rene <wllywlnt@yahoo.com>
+ */
+$lang['output']                = 'Toon het PDF Bestand in de browser, of vraag om het te downloaden?';
+$lang['usecache']              = 'Bewaar de PDF?';
+$lang['template']              = 'XSL template die standaard wordt gebruikt. Kan op individuele pagina\'s worden vervangen door <code>&tpl=name.xsl</code> aan het eind van de URL.
+';
+$lang['command']               = 'Het processor commando, waarbij <code>{xml}</code>, <code>{xsl}</code>, en <code>{pdf}</code> Worden vervangen door de correcte bestandssysteem paden.';
